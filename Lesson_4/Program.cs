97c1b54939b3f439810a3ac00f1e23cda3c11a3a
Anita <zorrodeplata.anita9@gmail.com>
2022-08-24 16:13:04 +0500

    Lesson 4 :wq

---
+++ Lesson_4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.WriteLine("Вывод максимального из трех чисел ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+int max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+Console.WriteLine(max);
